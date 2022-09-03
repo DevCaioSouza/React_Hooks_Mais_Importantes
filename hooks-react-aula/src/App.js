f0bdbd8beaedf900a74ae1d3207c9e8f58f6647d
unknown <caiocbahia@hotmail.com>
2022-09-03 14:01:18 -0300

    useState Hook

--- hooks-react-aula/src/App.js
+++ hooks-react-aula/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  //     state   função q altera o state
+  // const [count, setCount] = useState(0)
+
+  const [state, setState] = useState({ count: 0, theme: "light" });
+
+  // const incrementCount = () => {
+  //   setCount(count + 1)
+  // }
+
+  //a forma recomendada seria essa, com prevState em uma callback fn
+  const incrementCount = () => {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        count: prevState.count + 1
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{state.count}</h1>
+      <button onClick={incrementCount} >Incrementar</button>
     </div>
   );
-}
+};
 
 export default App;
+
+//useState armazena state nos nossos componentes funcionais
+//hooks só podem ser utilizados dentro de componentes funcionais
+
+//o n° de vezes q um hook é executado nunca pode mudar, então não
+//dá p colocar o hook em função, em if, etc
+
+//podemos usar objetos no useState
